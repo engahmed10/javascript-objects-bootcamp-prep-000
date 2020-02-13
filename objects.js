729b3066c9e251de69e3efb0e445276c96810414
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-13 10:27:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,6 @@ var playlist = { ibo : "haydisoyle" }
 function updatePlaylist(playlist, artistName, songTitle)
 {
   playlist[artistName]=songTitle;
-  // Object.assign({},playlist,{[artistName]:songTitle})
   return playlist
 }
 function removeFromPlaylist(playlist, artistName)
