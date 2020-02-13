c2c49e989e0736b612f5058244ffb827922ba14f
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-13 10:33:39 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,9 +4,9 @@ function updatePlaylist(playlist, artistName, songTitle)
   playlist[artistName]=songTitle;
   return playlist
 }
-function removeFromPlaylist(playlist1,artistName)
+function removeFromPlaylist(playlist,artistName)
 {
-  delete playlist1.artistName;
-  return playlist1
+  delete playlist.artistName;
+  return playlist
  
 }
